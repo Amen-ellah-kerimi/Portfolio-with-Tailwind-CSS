922beaf7b59a6f180e9bf71401ffd552fb04c240
User <user@example.com>
2025-07-07 11:39:19 +0100

    changed home to index and reconfigured the vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/Portfolio-with-Tailwind-CSS/',
     build: {
         rollupOptions: {
             input: {
-                home: resolve(__dirname, 'pages/Home.html'),
+                home: resolve(__dirname, 'pages/index.html'),
                 about: resolve(__dirname, 'pages/About.html'),
                 contacts: resolve(__dirname, 'pages/contacts.html'),
                 projects: resolve(__dirname, 'pages/Projects.html'),
